a745ebe2cc47ea6957ac094f83a95a1fa9640a18
DragonChenCL <chenlong_qrr@163.com>
2019-01-17 15:11:19 +0800

    新建项目

---
+++ dragonclub/src/main/java/com/dc/DragonClubApplication.java
@@ -0,0 +1,14 @@
+package com.dc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonClubApplication.class, args);
+    }
+
+}
+
